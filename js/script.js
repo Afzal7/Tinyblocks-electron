db9d70ff5ae9782b50e308289dab909741efe766
ankur panwar <ankur.panwar@w3villa.com>
2018-06-01 18:13:46 +0530

    design chrome app landing

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
   document.getElementById("app-navbar").style.width = "0px";
 }
+
+
+$(document).ready(function(){
+  $(document).on("click",'#landing-hide-btn',function(){
+      $(".landing-page").hide();
+  });
+});
